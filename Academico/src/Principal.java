b7844c07835d707a46e18eb175e5c33df8221769
Italoduraes <31115676+Italoduraes@users.noreply.github.com>
2017-08-17 22:22:02 -0300

    Update Principal.java

--- Academico/src/Principal.java
+++ Academico/src/Principal.java
@@ -6,14 +6,16 @@ public class Principal {
 		
 		//alunos
 		
-		Aluno a1= new Aluno("Italo duraes", "2179010" , 20 , "Ciência da Computação" , "Matutino" , "Masculino");
+		Aluno a1= new Aluno("Italo duraes", "2179010" , 20 , "CiÃªncia da ComputaÃ§Ã£o" , "Matutino" , "Masculino");
 		
 		//Disciplina
 		
-		Disciplina d1= new Disciplina("Algebra", "Ciência da Computação", 75);
+		Disciplina d1= new Disciplina("Algebra", "CiÃªncia da ComputaÃ§Ã£o", 75);
 		
 		
 		System.out.println(a1.getNome());
+		System.out.println(d1.getCurso());
+		
 		System.out.println(d1.getNome());
 		
 		System.out.println(a1.imprimirAluno());
